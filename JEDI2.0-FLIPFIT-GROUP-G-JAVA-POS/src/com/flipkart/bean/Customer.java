26909b761ddadf37455a63aab28739a5740e56f4
SriLoukyaPappula <pappula.sriloukya@flipkart.com>
2024-07-19 15:19:40 +0530

    customer bean

--- JEDI2.0-FLIPFIT-GROUP-G-JAVA-POS/src/com/flipkart/bean/Customer.java
+++ JEDI2.0-FLIPFIT-GROUP-G-JAVA-POS/src/com/flipkart/bean/Customer.java
@@ -6,32 +6,64 @@ package com.flipkart.bean;
 /**
  * 
  */
-public class Customer{
-	private String contactNo;
-	private String gender;
-	private String customerName;
+public class Customer {
 
-	public String getContactNo() {
-		return contactNo;
+	private int customerId;         // Unique identifier for the customer
+	private String customerName;    // Name of the customer
+	private String customerPhone;   // Phone number of the customer
+	private String customerAddress; // Address of the customer
+	private int userId;             // Identifier of the associated user
+
+
+	public Customer(int customerId, String customerName, String customerPhone, String customerAddress, int userId) {
+		this.customerId = customerId;
+		this.customerName = customerName;
+		this.customerPhone = customerPhone;
+		this.customerAddress = customerAddress;
+		this.userId = userId;
 	}
 
-	public void setContactNo(String contactNo) {
-		this.contactNo = contactNo;
+	public int getCustomerId() {
+		return customerId;
 	}
 
-	public String getGender() {
-		return gender;
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
 	}
 
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	
-	public void setcustomerName(String customerName) {
-		this.customerName=customerName;
-	}
-	public String getcustomerName() {
+	public String getCustomerName() {
 		return customerName;
 	}
 
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerPhone() {
+		return customerPhone;
+	}
+
+
+	public void setCustomerPhone(String customerPhone) {
+		this.customerPhone = customerPhone;
+	}
+
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+
+
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
 }
+
